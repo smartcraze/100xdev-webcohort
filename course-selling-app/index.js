75fa1812b65bdc8e25e114f76786583a3a2adf75
smartcraze <smartcraze354@gmail.com>
2024-12-29 06:02:10 +0000

    course selling app init

---
+++ course-selling-app/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+
+const app = express();
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is Running on Port 3000 ⚙️ http://localhost:3000 ');
+
+})
