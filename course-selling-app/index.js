f68c9e9e1b8a249182b2128b1e52066a4d16176f
smartcraze <surajkv9973@gmail.com>
2024-12-29 11:51:21 +0530

    adding routes

--- course-selling-app/index.js
+++ course-selling-app/index.js
@@ -3,6 +3,41 @@ const express = require('express')
 
 const app = express();
 
+// signup
+app.post('/user/signup', async function (req, res) {
+    res.json({
+        msg: "user signup"
+    })
+})
+// sign in 
+app.post('/user/signin', async function (req, res) {
+    res.json({
+        msg: "user signin"
+    })
+})
+
+//  when user is purchasing the course
+app.post('/course/purchase', async function (req, res) {
+    // user should pay for purchase courses
+    res.json({
+        msg: "user purchasing course"
+    })
+})
+
+// to get all the user purchased course
+app.get('/user/purchase', async function (req, res) {
+    res.json({
+        msg: "user course"
+    })
+})
+
+//to get all the course availabel
+app.get('/course', async function (req, res) {
+    res.json({
+        msg: "all the course"
+    })
+})
+
 
 
 
