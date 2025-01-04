02c0fd1a72bc652d7dd674d69ba3a77fca4b7019
smartcraze <smartcraze354@gmail.com>
2025-01-04 12:59:31 +0530

    testing comit

--- Advanced-ts-api/src/index.ts
+++ Advanced-ts-api/src/index.ts
@@ -1,18 +1,18 @@
 interface User {
   name: string;
   age: number;
-  email:string;
-  phone:string;
-  LastName:string;
+  email: string;
+  phone: string;
+  LastName: string;
 }
 
 type PartialUser = Partial<User>;
 type ReadonlyUser = Readonly<User>;
-type Updatedprops = Pick<User, 'name' | 'age'>;
+type Updatedprops = Pick<User, "name" | "age">;
 
 // Example usage
 const partialUser: PartialUser = {
-  name: "John"
+  name: "John",
   // other properties are optional
 };
 
@@ -21,12 +21,14 @@ const readonlyUser: ReadonlyUser = {
   age: 30,
   email: "jane@example.com",
   phone: "123-456-7890",
-  LastName: "Doe"
+  LastName: "Doe",
 };
 // readonlyUser.name = "John"; // Error: Cannot assign to 'name' because it is a read-only property.
 
 const updatedProps: Updatedprops = {
   name: "Alice",
-  age: 25
+  age: 25,
   // only 'name' and 'age' are allowed
 };
+
+console.log(partialUser);
