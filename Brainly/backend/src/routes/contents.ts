82fd67113236fb37a4e481665fd37a076f050c82
smartcraze <smartcraze354@gmail.com>
2025-01-05 19:58:52 +0530

    added express userid types

--- Brainly/backend/src/routes/contents.ts
+++ Brainly/backend/src/routes/contents.ts
@@ -1,3 +1,29 @@
 import { Router } from "express";
+import { z } from "zod";
+import auth from "../middleware/auth";
+import Content from "../Models/contents";
 
 export const contentsRouter = Router();
+
+const ContentValidationSchema = z.object({
+  link: z.string().url(),
+  type: z.enum(["image", "video", "article", "audio"]),
+  title: z.string().min(1),
+  //   tags: z.array(z.string()),
+});
+
+contentsRouter.post("/add", auth, async (req, res) => {
+  try {
+    const { link, type, title } = ContentValidationSchema.parse(req.body);
+    await Content.create({
+      link,
+      type,
+      title,
+      tags: [],
+      userId: req.userId,
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ Brainly/backend/types/express.d.ts
@@ -0,0 +1,7 @@
+// types/express.d.ts
+
+declare namespace Express {
+  export interface Request {
+    userId?: string; 
+  }
+}
